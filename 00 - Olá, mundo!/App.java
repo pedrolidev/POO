db808aa5e097234be6d96aa4cfc5ba5437cd19e6
Pedro Oliveira <126485301+ppedr0liveira@users.noreply.github.com>
2023-08-11 15:47:34 -0300

    Add files via upload

---
+++ 00 - Olá, mundo!/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Comentario
+         * de
+         * bloco
+         */
+        System.out.println("Hello, World!"); // Comentario de linha
+    }
+}
